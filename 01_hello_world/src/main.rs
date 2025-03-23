c1b99d4762187d9a45ef8b3aa153e4ee66764daa
Pragneshpatel2023 <patelpragnesh036@gmail.com>
2025-03-23 16:33:55 +0530

    Print Hello world project.

---
+++ 01_hello_world/src/main.rs
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : main.rs
+// Author      : Pragnesh Patel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Testing code for Rust
+//============================================================================
+
+fn main()
+{
+    println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    print!("Training Of rust\r\n");
+    println!("Hello world rust");
+    print!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+}
+
