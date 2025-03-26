8993e27e3cda620136acb32cf12c08da0b5223fa
Pragneshpatel2023 <patelpragnesh036@gmail.com>
2025-03-26 23:03:41 +0530

    Included comments and formatted the code using the 'cargo fmt' command.

--- 01_hello_world/src/main.rs
+++ 01_hello_world/src/main.rs
@@ -4,13 +4,15 @@
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Testing code for Rust
+// We can create a project using "cargo new".
+// We can build a project using "cargo build".
+// We can build and run a project in one step using "cargo run".
+// We can build a project without producing a binary to check for errors using "cargo check"
 //============================================================================
 
-fn main()
-{
+fn main() {
     println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     print!("Training Of rust\r\n");
     println!("Hello world rust");
     print!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 }
-
